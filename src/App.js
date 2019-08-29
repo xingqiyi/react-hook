44d53fae0ae12672e50fb55a93119ec42ad5f71f
shuaixiaocheng <448378892@qq.com>
2019-08-29 16:33:56 +0800

    增加react-router

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+// import { Router, Route, Link } from 'react-router'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,14 +8,37 @@ import DataFetch from './components/DataFetch'
 import CustomDataFetch from './components/CustomDataFetch'
 import ReducerDataFetch from './components/ReducerDataFetch'
 
+const PrimaryLayout = () => (
+  <div className="primary-layout">
+    <header>
+      Our React Router 4 App
+    </header>
+    <main>
+      {/* 添加 exact 属性，否则 / 会被 /users 或者 /users/add */}
+      <Route path="/" exact component={DataFetch} />
+      <Route path="/a" exact component={DataFetch} />
+      <Route path="/b" component={CustomDataFetch} />
+      <Route path='/c' component={ReducerDataFetch} />
+
+    </main>
+  </div>
+)
+
+
 function App() {
+  // return (
+  //   <div className="App">
+  //     <ReducerDataFetch></ReducerDataFetch>
+  //   </div>
+  // );
+
   return (
-    <div className="App">
-
-      <ReducerDataFetch></ReducerDataFetch>
-
-    </div>
+    <BrowserRouter>
+      <PrimaryLayout />
+    </BrowserRouter>
   );
+
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Router, Route, Link } from 'react-router'
+
+
+
+
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
